659d4d3f7a40ba7cd1e3f5b8a34446a7564d34fc
Rahul Sompuram <sompuram@sas.upenn.edu>
2020-05-08 16:31:00 -0400

    work section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
+import Work from './Work.js';
 import TypeIt from 'typeit';
+import sun from './sun.png';
+import moon from './moon.png';
+import song from './song.mp3';
 
 export default class App extends React.Component {
 
@@ -8,7 +12,8 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
-      isClicked: false
+      isClicked: false,
+      isWorkClicked: false,
     }
   }
 
@@ -24,37 +29,73 @@ export default class App extends React.Component {
     }).go();
   }
 
-  onNameClick = (e) => {
+  onNameClick = () => {
     this.setState({
       isClicked: !this.state.isClicked
+    })
+  }
+
+  onWorkClick = () => {
+    this.setState({
+      isWorkClicked: !this.state.isWorkClicked,
     })
   }
 
   render() {
     return (
       <div className="App" style={{backgroundColor: this.state.isClicked ? "#243447" : "#FFFFFF"}}>
-        <header className="App-header" style={{backgroundColor: this.state.isClicked ? "#243447" : "#FFFFFF"}}>
-          <div className="masthead">
-            <h2>
-              <span onClick={this.onNameClick} id="name" className="blue-text">rahul sompuram</span>
-              <br/>
-              <small style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>designer. developer. doer.</small>
-            </h2>
-          </div>
-        </header>
-        <div className="main-container">
-          <div className="about">
-            <h3 id="replaceStrings">hi!</h3>
-            <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>I'm Rahul, a senior at the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">University of Pennsylvania</span> studying computer science and economics.
-            This past summer, I was a <span><a href="http://fellows.kleinerperkins.com/meet-the-fellows" target="_blank" rel="noopener noreferrer">Kleiner Perkins Engineering Fellow</a></span> working on full-stack software engineering and data science projects at <span><a href="https://www.betterworks.com" target="_blank" rel="noopener noreferrer">BetterWorks</a></span>.
-            I started <span className="blue-text"><a href="https://www.34st.com/article/2019/02/ego-rahul-sompuram-buzzhealth" target="_blank" rel="noopener noreferrer">Buzz Health</a></span>, a mobile resource app for patients with serious or chronic conditions and their supporters. We've been grantees of
-            the 1517 Fund, finalists for Y-Combinator's W'19 batch, and have received strong interest from top U.S. hospitals. </p>
-            <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>In my free time, I enjoy swinging my racquet on the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">tennis</span> courts, playing my <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">saxophone</span> (jazz, funk, neo-soul),
-            and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">traveling</span> to new places (I most recently road-tripped around all of Iceland).</p>
-            <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>My career interests lie in <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">product management</span> and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">software engineering</span>. I love connecting with fellow explorers, creators, and dreamers so please don't hesitate to reach out!</p>
-            <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="connect"><span><a href="https://www.linkedin.com/in/rahulsompuram" target="_blank" rel="noopener noreferrer">LinkedIn</a></span>, <span><a href="https://github.com/rahulsompuram" target="_blank" rel="noopener noreferrer">Github</a></span>, <span><a href="mailto:me@rahulsompuram.com">Email</a></span></p>
-          </div>
-        </div>
+        {
+          this.state.isWorkClicked ?
+            <div>
+              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end' }}>
+                <a href="#" onClick={this.onWorkClick}>
+                  <h3 id="work" className="blue-text" style={{ fontSize: 20, margin: 0, paddingTop: 25, paddingRight: 25 }}>
+                    {this.state.isWorkClicked ? "about": "work"}
+                  </h3>
+                </a>
+              </div>
+              <Work isClicked={this.state.isClicked} />
+            </div>
+              :
+            <div>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <img style={{ paddingTop: 25, paddingLeft: 25 }} onClick={this.onNameClick} src={this.state.isClicked ? moon : sun} alt="sun" height='30' width='30' />
+              <a href="#" onClick={this.onWorkClick}>
+                <h3 id="work" className="blue-text" style={{ fontSize: 20, margin: 0, paddingTop: 25, paddingRight: 25 }}>
+                  {this.state.isWorkClicked ? "about": "work"}
+                </h3>
+              </a>
+            </div>
+              <header className="App-header" style={{backgroundColor: this.state.isClicked ? "#243447" : "#FFFFFF"}}>
+                <div className="masthead">
+                  <h2>
+                    <span id="name" className="blue-text">rahul sompuram</span>
+                    <br/>
+                    <small style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>designer. developer. doer.</small>
+                  </h2>
+                </div>
+              </header>
+              <div className="main-container" style={{ paddingRight: 25, paddingLeft: 25 }}>
+                <div className="about">
+                  <h3 id="replaceStrings">hi!</h3>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>I'm Rahul, a senior at the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">University of Pennsylvania</span> studying computer science and economics.
+                  This past summer, I was a <span><a id="underline" href="http://fellows.kleinerperkins.com/meet-the-fellows" target="_blank" rel="noopener noreferrer">Kleiner Perkins Engineering Fellow</a></span> working on full-stack software engineering and data science projects at <span><a id="underline" href="https://www.betterworks.com" target="_blank" rel="noopener noreferrer">BetterWorks</a></span>.
+                  I started <span className="blue-text"><a id="underline" href="https://www.34st.com/article/2019/02/ego-rahul-sompuram-buzzhealth" target="_blank" rel="noopener noreferrer">Buzz Health</a></span>, a mobile resource app for patients with serious or chronic conditions and their supporters. We've been grantees of
+                  the 1517 Fund, finalists for Y-Combinator's W'19 batch, and have received strong interest from top U.S. hospitals. </p>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>In my free time, I enjoy swinging my racquet on the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">tennis</span> courts, playing my <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">saxophone</span> (jazz, funk, neo-soul),
+                  and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">traveling</span> to new places (I most recently road-tripped around all of Iceland).</p>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>My career interests lie in <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">product management</span> and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">software engineering</span>. I love connecting with fellow explorers, creators, and dreamers so please don't hesitate to reach out!</p>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="connect"><span><a id="underline" href="https://www.linkedin.com/in/rahulsompuram" target="_blank" rel="noopener noreferrer">LinkedIn</a></span>, <span><a id="underline" href="https://github.com/rahulsompuram" target="_blank" rel="noopener noreferrer">Github</a></span>, <span><a id="underline" href="mailto:me@rahulsompuram.com">Email</a></span></p>
+                </div>
+                <div style={{ paddingTop: 25, paddingBottom: 25, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                  <h4 style={{ fontWeight: 600, fontFamily: 'Inter', color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>Song of the week: <span style={{ fontWeight: 500, fontStyle: "italic" }}>Feel (feat. Lianne La Havas) - Jacob Collier</span></h4>
+                  <audio controls>
+                    <source src={song} type="audio/mpeg" />
+                  </audio>
+                </div>
+              </div>
+            </div>
+        }
       </div>
     );
   }
---
+++ src/Work.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import './App.css';
+import tennis from './tennis.png';
+import pennbook from './pennbook.png';
+import breadcrumbs from './breadcrumbs.png';
+import vidsearch from './vidsearch.png';
+import stayhome from './stayhome.png';
+import raft from './raft.png';
+import award from './award.png';
+
+
+export default class Work extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: props.isClicked
+    }
+  }
+
+  render() {
+    return (
+      <div className="main-container" style={{ paddingRight: 25, paddingLeft: 25 }}>
+        <div className="work" style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+          <div style={{ alignSelf: 'center', fontFamily: 'Muli', fontWeight: 700, fontSize: 24 }}>
+            <h2><span className="blue-text">some work</span></h2>
+          </div>
+          <div style={{ paddingBottom: 50 }}>
+            <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700 }}>Volleys & Returns</h1>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>A web app that allows tennis fans to improve their betting decisions.</h4>
+            <img src={tennis} alt="Volleys & Returns" height='100%' width='100%' />
+            <br/>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>React, MaterialUI, Express, Pandas, SQL Developer, RDS</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>CIS 550 (graduate databases) final project</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>4</h4>
+            <a id="underline-green" style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: 700 }} href="https://www.youtube.com/watch?v=DahoEc1vf8U&feature=youtu.be" target="_blank" rel="noopener noreferrer">Learn more</a>
+          </div>
+
+          <div style={{ paddingBottom: 50 }}>
+            <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700  }}>Stay Home</h1>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>An iOS game that incentivizes users with rewards to stay home during the COVID-19 pandemic.</h4>
+            <img src={stayhome} alt="Stay Home" height='100%' width='100%' />
+            <br/>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>Swift5, SwiftUI, Firebase</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Design: </span>Figma</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>Open-source project to help mitigate the spread of COVID-19</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>4</h4>
+            <a id="underline-pink" style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: 700 }} href="https://devpost.com/software/stay-home-pyqn19" target="_blank" rel="noopener noreferrer">Learn more</a>
+          </div>
+
+          <div style={{ paddingBottom: 50 }}>
+            <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700  }}>Raft Health (formerly Buzz Health)</h1>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>A mobile resource app for patients going through serious illnesses and their supporters.</h4>
+            <img src={raft} alt="Raft Health" height='100%' width='100%' />
+            <br/>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>React Native (for v1), Swift5 (for v2), Firebase</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Design: </span>Figma</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>Passion project and business venture</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>2</h4>
+            <a id="underline-purple" style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: 700 }} href="https://www.34st.com/article/2019/02/ego-rahul-sompuram-buzzhealth" target="_blank" rel="noopener noreferrer">Learn more</a>
+          </div>
+
+          <div style={{ paddingBottom: 50 }}>
+            <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700  }}>Pennbook</h1>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>A scalable Facebook clone with news feed, friend recommendations, messaging, and profile pages.</h4>
+            <img src={pennbook} alt="Pennbook" height='100%' width='100%' />
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>React, Semantic UI, Express, Socket IO, Hadoop, Dynamo DB, S3, EC2</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>NETS 212 (scalable & cloud computing) final project</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>3</h4>
+          </div>
+
+          <div style={{ paddingBottom: 50 }}>
+            <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700  }}>Breadcrumbs</h1>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>A collaborative rich-text editing desktop app that allows users to create, view, edit, and share documents.</h4>
+            <img src={breadcrumbs} alt="Breadcrumbs" height='100%' width='100%' />
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>Electron, Semantic UI, Express, Socket IO, MongoDB</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>Horizons School of Technology project</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>4</h4>
+          </div>
+
+          <div style={{ paddingBottom: 50 }}>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+              <h1 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Muli', fontWeight: 700  }}>VidSearch</h1>
+              <img src={award} alt="award" height='30' width='30' />
+            </div>
+            <h4 style={{ fontSize: 18, color: this.state.isClicked ? '#D3D3D3' : '#87898C', fontFamily: 'Inter', fontWeight: 600 }}>A web app that gives users CTRL-F functionality for videos.</h4>
+            <img src={vidsearch} alt="VidSearch" height='100%' width='100%' />
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Tech stack: </span>React, Express, Google Speech-to-Text API</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Purpose: </span>Horizons School of Technology hackathon</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Team size: </span>5</h4>
+            <h4 style={{ color: this.state.isClicked ? "#FFFFFF" : "#585858", fontFamily: 'Inter', fontSize: 16, fontWeight: 500 }}><span style={{ fontWeight: 700 }}>Award: </span>2nd place for "Best Overall"</h4>
+            <a id="underline" style={{ fontFamily: 'Inter', fontSize: 16, fontWeight: 700 }} href="https://drive.google.com/file/d/18zbb3dnpaaiMsMQbcltOG27nVfbG2lag/view" target="_blank" rel="noopener noreferrer">Learn more</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
