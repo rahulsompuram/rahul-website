6c40b5cefa99f92f30027d9d974f43e33f0c097e
Rahul Sompuram <sompuram@sas.upenn.edu>
2020-05-08 17:03:01 -0400

    Updates

---
+++ precache-manifest.4bfc0eb3b7970b9a9ad7c3b76559be34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43783f329694643e82e2a2471c57bc45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dbb1af990082d52a1a0",
+    "url": "/static/css/main.4d275b26.chunk.css"
+  },
+  {
+    "revision": "993615c69003f592d9f6",
+    "url": "/static/js/2.d1688075.chunk.js"
+  },
+  {
+    "revision": "8dbb1af990082d52a1a0",
+    "url": "/static/js/main.b2e3c758.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c1bca535713f657c0f17bfb3f5140bc",
+    "url": "/static/media/breadcrumbs.3c1bca53.png"
+  },
+  {
+    "revision": "8abd7037e50da551e0a8404fac66c007",
+    "url": "/static/media/pennbook.8abd7037.png"
+  },
+  {
+    "revision": "e2dbc70d2deccb2f33625497087d8624",
+    "url": "/static/media/raft.e2dbc70d.png"
+  },
+  {
+    "revision": "dfbdf830f9e0eedfb0bb82d685e9ff3e",
+    "url": "/static/media/song.dfbdf830.mp3"
+  },
+  {
+    "revision": "791234944616369e00d9d745434c23ff",
+    "url": "/static/media/stayhome.79123494.png"
+  },
+  {
+    "revision": "e43d59a85b5324e9652483c2754b7c0f",
+    "url": "/static/media/tennis.e43d59a8.png"
+  },
+  {
+    "revision": "432586d5bc2bba5a8294d6bd516f7f5e",
+    "url": "/static/media/vidsearch.432586d5.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.865b60f764a8966b8afddb60cf5147ab.js"
+  "/precache-manifest.4bfc0eb3b7970b9a9ad7c3b76559be34.js"
 );
 
 self.addEventListener('message', (event) => {
