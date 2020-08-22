d558d86014b62b3b1524b5f6f1781a7aeabb3f23
Rahul Sompuram <sompuram@sas.upenn.edu>
2020-08-22 11:54:28 -0400

    first job update

--- src/App.js
+++ src/App.js
@@ -70,24 +70,24 @@ export default class App extends React.Component {
                   <h2>
                     <span id="name" className="blue-text">rahul sompuram</span>
                     <br/>
-                    <small style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>designer. developer. doer.</small>
+                    {/*<small style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>designer. developer. doer.</small>*/}
                   </h2>
                 </div>
               </header>
               <div className="main-container" style={{ paddingRight: 25, paddingLeft: 25 }}>
                 <div className="about">
                   <h3 id="replaceStrings">hi!</h3>
-                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>I'm Rahul, a senior at the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">University of Pennsylvania</span> studying computer science and economics.
-                  This past summer, I was a <span><a id="underline" href="http://fellows.kleinerperkins.com/meet-the-fellows" target="_blank" rel="noopener noreferrer">Kleiner Perkins Engineering Fellow</a></span> working on full-stack software engineering and data science projects at <span><a id="underline" href="https://www.betterworks.com" target="_blank" rel="noopener noreferrer">BetterWorks</a></span>.
-                  I started <span className="blue-text"><a id="underline" href="https://www.34st.com/article/2019/02/ego-rahul-sompuram-buzzhealth" target="_blank" rel="noopener noreferrer">Buzz Health</a></span>, a mobile resource app for patients with serious or chronic conditions and their supporters. We've been grantees of
-                  the 1517 Fund, finalists for Y-Combinator's W'19 batch, and have received strong interest from top U.S. hospitals. </p>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>I'm Rahul, a product manager working on user growth at <span className="blue-text"><a id="underline" href="https://www.redfin.com/" target="_blank" rel="noopener noreferrer">Redfin</a></span>. I recently graduated from the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">University of Pennsylvania</span> with a degree in computer science and economics.
+                  In the past, I was a <span><a id="underline" href="http://fellows.kleinerperkins.com/meet-the-fellows" target="_blank" rel="noopener noreferrer">Kleiner Perkins Engineering Fellow</a></span> at <span><a id="underline" href="https://www.betterworks.com" target="_blank" rel="noopener noreferrer">BetterWorks</a></span>.
+                  I started <span className="blue-text"><a id="underline" href="https://www.34st.com/article/2019/02/ego-rahul-sompuram-buzzhealth" target="_blank" rel="noopener noreferrer">Raft Health (formerly Buzz Health)</a></span>, a mobile resource app for patients with serious or chronic conditions and their supporters. We were grantees of
+                  the 1517 Fund, finalists for Y-Combinator's W'19 batch, and worked with a couple of the top U.S. pediatric hospitals. </p>
                   <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>In my free time, I enjoy swinging my racquet on the <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">tennis</span> courts, playing my <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">saxophone</span> (jazz, funk, neo-soul),
-                  and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">traveling</span> to new places (I most recently road-tripped around all of Iceland).</p>
-                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>My career interests lie in <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">product management</span> and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">software engineering</span>. I love connecting with fellow explorers, creators, and dreamers so please don't hesitate to reach out!</p>
+                  and <span style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="gray-text">traveling</span> to new places (RIP ~covid-19~).</p>
+                  <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>I love connecting with fellow explorers, creators, and dreamers so please don't hesitate to reach out!</p>
                   <p style={{color: this.state.isClicked ? "#FFFFFF" : "#585858"}} className="connect"><span><a id="underline" href="https://www.linkedin.com/in/rahulsompuram" target="_blank" rel="noopener noreferrer">LinkedIn</a></span>, <span><a id="underline" href="https://github.com/rahulsompuram" target="_blank" rel="noopener noreferrer">Github</a></span>, <span><a id="underline" href="mailto:me@rahulsompuram.com">Email</a></span></p>
                 </div>
                 <div style={{ paddingTop: 5, paddingBottom: 25, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                  <h4 style={{ fontStyle: "italic", fontWeight: 600, fontFamily: 'Inter', color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>Currently listening to: <span style={{ fontWeight: 500 }}>Feel (feat. Lianne La Havas) - Jacob Collier</span></h4>
+                  <h4 style={{ fontStyle: "italic", fontWeight: 600, fontFamily: 'Inter', color: this.state.isClicked ? "#FFFFFF" : "#585858"}}>Currently listening to: <span style={{ fontWeight: 500 }}>All of the Lights - Alexander Lewis, Brasstracks</span></h4>
                 </div>
               </div>
             </div>
